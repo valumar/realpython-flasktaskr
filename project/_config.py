acd0b5aa08233b136d924d18ca81fc8ee594c083
Petr Antonov <antpeter@gmail.com>
2017-07-14 19:58:20 +0300

    Deploy

---
+++ project/_config.py
@@ -0,0 +1,16 @@
+import os
+
+# grab the folder where this script lives
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+DATABASE = 'flasktaskr.db'
+WTF_CSRF_ENABLED = True
+SECRET_KEY = 'my_precious'
+
+# define the full path for the database
+DATABASE_PATH = os.path.join(basedir, DATABASE)
+
+# the database uri
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + DATABASE_PATH
+
+DEBUG = False
