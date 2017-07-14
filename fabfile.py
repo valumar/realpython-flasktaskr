f9037ca4056c37f708d286f7137dae5981d933cf
Petr Antonov <antpeter@gmail.com>
2017-07-14 20:31:06 +0300

    Testing Fabric3

---
+++ fabfile.py
@@ -0,0 +1,21 @@
+from fabric.api import local, settings, abort
+from fabric.contrib.console import confirm
+
+
+def test():
+    local("nosetests -v")
+
+
+def commit():
+    message = input("Enter a git commit message: ")
+    local("git add . && git commit -am '{}'".format(message))
+
+
+def push():
+    local("git push origin master")
+
+
+def prepare():
+    test()
+    commit()
+    push()
